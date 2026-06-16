94fd00e8b9612c4f9c7c3b60903dd45d231db64d
cth.release <cth.release@gmail.com>
2026-06-16 13:15:33 +0900

    feat: Embed web editor shell assets and add coverage

---
+++ pkg/plugin/editor_assets_test.go
@@ -0,0 +1,29 @@
+package plugin
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestBuildEditorHTMLInjectsAssetsAndSnapshot(t *testing.T) {
+	html := buildEditorHTML("dGVzdC1zbmFwc2hvdA==")
+
+	if strings.Contains(html, "__VST3GO_CSS__") {
+		t.Fatal("CSS placeholder was not replaced")
+	}
+	if strings.Contains(html, "__VST3GO_JS__") {
+		t.Fatal("JS placeholder was not replaced")
+	}
+	if strings.Contains(html, "__VST3GO_SNAPSHOT__") {
+		t.Fatal("snapshot placeholder was not replaced")
+	}
+	if !strings.Contains(html, "dGVzdC1zbmFwc2hvdA==") {
+		t.Fatal("encoded snapshot was not embedded")
+	}
+	if !strings.Contains(html, ".shell") {
+		t.Fatal("embedded CSS was not included")
+	}
+	if !strings.Contains(html, "window.__vst3goUpdateParameter") {
+		t.Fatal("embedded JS was not included")
+	}
+}
